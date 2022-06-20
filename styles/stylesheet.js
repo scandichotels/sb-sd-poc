7af6c1b95d4b13e2603180ad10ffc1e26460770f
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2022-06-20 11:05:22 +0200

    added material ui global in storybook

---
+++ styles/stylesheet.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+// import { tokenRed } from "../styles/design-tokens";
+
+export const muiTheme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: tokenRed,
+  //   },
+  // },
+});
